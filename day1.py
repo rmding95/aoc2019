8b5246013d736163236064191d9de92d3c5891b3
Richard Ding <rmding2013@gmail.com>
2019-12-01 20:37:10 -0800

    Day 1 Solution

---
+++ day1.py
@@ -0,0 +1,24 @@
+INPUT_FILENAME = "day1input.txt"
+
+
+def main():
+    total_mass = 0
+    fuel_mass = 0
+    with open(INPUT_FILENAME) as f:
+        for line in f:
+            total_mass += (int(line) // 3) - 2
+            fuel_mass += calculate_fuel(int(line))
+    print("part 1: " + str(total_mass))
+    print("part 2: " + str(fuel_mass))
+
+
+def calculate_fuel(mass):
+    fuel = (mass // 3) - 2
+    if fuel < 0:
+        return 0
+    print(fuel)
+    return fuel + calculate_fuel(fuel)
+
+
+if __name__ == "__main__":
+    main()
